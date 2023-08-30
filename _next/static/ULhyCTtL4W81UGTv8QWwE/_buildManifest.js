f52ac104ec8f6072b803850a11b556eea3e37c52
Jörgen Thelin <thelin.jorgen@gmail.com>
2023-08-30 21:58:59 +0200

    Updates

--- _next/static/ULhyCTtL4W81UGTv8QWwE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-8883a755e518fd5e3c27.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8674db961288683ef4e0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
