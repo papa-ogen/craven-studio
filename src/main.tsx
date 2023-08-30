8322cf01bae80715e492276c650745a23ea70b55
Jörgen Thelin <thelin.jorgen@gmail.com>
2023-08-30 21:56:33 +0200

    added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./index.css";
+
 import "../node_modules/@papa-ogen/craven-ui/dist/style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
