f3e5087a3fb8d0e12ea65f1388071c758d1b8c98
Jörgen Thelin <thelin.jorgen@gmail.com>
2024-08-31 16:29:00 +0200

    gsap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
 import "./input.css";
+import { gsap } from "gsap";
+import TextPlugin from "gsap/TextPlugin";
 
-console.log("TODO: dynamic menu");
+gsap.registerPlugin(TextPlugin);
+
+const element = document.getElementById("developer-title");
+if (element) {
+  gsap.fromTo(
+    element,
+    { text: "D------r" }, // Start with scrambled text
+    {
+      text: "Developer", // End with original text
+      duration: 2,
+      ease: "power1.inOut",
+    }
+  );
+}
